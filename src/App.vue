66a72fa8e88be158d920bc930ad9f3991281f56c
illia_kots <illyahennadievich@gmail.com>
2023-10-13 13:32:51 +0300

    Add starting code

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div className="todoapp">
+    <h1 className="todoapp__title">todos</h1>
+
+    <h2 className="todoapp__userTitle">{getUserName()}</h2>
+
+    <div className="todoapp__content">
+      <Header
+        userId={USER_ID}
+        createdTodo={handleAddTodo}
+        isLoading={isLoading}
+        todos={todos}
+        isCheckedToggleAll={isCheckedToggleAll}
+        toggleAllTodos={toggleAllTodos}
+      />
+
+      <TodoList
+        todos={visibleTodos}
+        tempTodo={tempTodo}
+        processedTodos={processedTodos}
+        onUpdateTodo={handleUpdateTodo}
+        onUpdateTitle={handleUpdateTitle}
+        onDelete={handleDeleteTodo}
+      />
+
+      {!!todos.length && (
+        <Footer
+          activeTodos={activeTodos}
+          completedTodos={completedTodos}
+          onDelete={handleDeleteTodo}
+        />
+      )}
+    </div>
+
+    {error && (
+      <Notifications
+        setError={setError}
+        error={error}
+      />
+    )}
+  </div>
+</template>
